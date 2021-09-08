c237505b3a77fe935a0299a7f729567ce1ee675a
Kur0den <kame241612@gmail.com>
2021-09-08 17:10:58 +0900

    てts

--- discordbot.py
+++ discordbot.py
@@ -2,6 +2,7 @@ from discord.ext import commands
 import discord
 import os
 import traceback
+import inspect
 
 bot = commands.Bot(command_prefix='k/')
 token = os.environ['DISCORD_BOT_TOKEN']
@@ -16,6 +17,7 @@ async def on_ready():
     user = client.get_user(699414261075804201)
     await user.send('きどうしたよ！！！！！！！ほめて！！！！！！！！')
 
+
 @client.command(name='eval', pass_context=True)
 @commands.is_owner()
 async def eval_(ctx, *, command):
@@ -24,7 +26,8 @@ async def eval_(ctx, *, command):
         await ctx.send(await res)
     else:
         await ctx.send(res)
-        
+
+
 @bot.event
 async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
