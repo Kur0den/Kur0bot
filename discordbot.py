32de6b32452ebd1e72daec644cae7efc1b231b81
Kur0den0010 <69957747+Kur0den@users.noreply.github.com>
2021-09-08 13:48:16 +0900

    pingggggggggg

--- discordbot.py
+++ discordbot.py
@@ -15,7 +15,7 @@ async def on_command_error(ctx, error):
 
 @bot.command()
 async def ping(ctx):
-    await ctx.send('pong')
+    await ctx.send(f":ping_pong:Pong! {round(bot.latency * 1000)}ms")
 
 
 bot.run(token)
