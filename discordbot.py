598d57c68d779d386a1dfc8a26cba7512ee5b1d6
Kur0den0010 <69957747+Kur0den@users.noreply.github.com>
2021-09-08 15:43:40 +0900

    起動メッセージ追加

--- discordbot.py
+++ discordbot.py
@@ -6,6 +6,10 @@ import traceback
 bot = commands.Bot(command_prefix='k/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
+@client.event
+async def on_ready():
+    user = client.get_user(699414261075804201)
+    await user.send('きどうしたよ！！！！！！！ほめて！！！！！！！！')
 
 @bot.event
 async def on_command_error(ctx, error):
