fe8bdae31d0c8f0110f818a5b335c109971627ee
Kur0den0010 <69957747+Kur0den@users.noreply.github.com>
2021-09-10 11:28:45 +0900

    importしゅうせい

--- discordbot.py
+++ discordbot.py
@@ -2,7 +2,7 @@ from discord.ext import commands
 import discord
 import os
 import traceback
-import datatime
+import datetime
 
 bot = commands.Bot(command_prefix='k/', intents=discord.Intents.all())
 token = os.environ['DISCORD_BOT_TOKEN']
