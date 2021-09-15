4d58c8a79c2ac65a1bd2197e38717c9208e5a183
Kur0den <kame241612@gmail.com>
2021-09-15 09:35:10 +0900

    bot

--- discordbot.py
+++ discordbot.py
@@ -84,7 +84,7 @@ async def idinfo(ctx, imid):
     iid = guild.get_channel(imid)
     # ty = 'チャンネル又はスレッドID'
     if iid == None:
-        iid = guild.get_role(imid)
+        iid = bot.get_role(imid)
     
     if iid == None:
         iid == guild.get_emoji(imid)
