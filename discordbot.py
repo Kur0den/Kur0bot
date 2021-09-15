01c35cba349f0c5582ae419138707c1a9087bb1e
Kur0den <kame241612@gmail.com>
2021-09-15 11:10:01 +0900

    test

--- discordbot.py
+++ discordbot.py
@@ -94,24 +94,17 @@ async def idinfo(ctx, imid):
     
     if iid == None:
         try:
-            iid = await bot.fetch_sticker(imid)
-            # ty = 'ステッカーID'
-        except discord.NotFound:
-            tid = 1
-            
-    if tid == 1:
-        try:
             iid = await bot.fetch_user(imid)
             # ty = 'ユーザーID'
         except discord.NotFound:
-            eid = 2
-    if tid == 2:
+            eid = 1
+    if tid == 1:
         try:
             iid = await bot.fetch_guild(imid)
             # ty = 'サーバーID'
         except:
-            tid = 3
-    if tid == 3:
+            tid = 2
+    if tid == 2:
             iid = None
     try:
         ex_name = iid.name
