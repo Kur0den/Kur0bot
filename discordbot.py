f089f9549e61175585158c7f412369b289b8dd19
Kur0den <kame241612@gmail.com>
2021-09-16 00:04:24 +0900

    TST追加

--- discordbot.py
+++ discordbot.py
@@ -64,16 +64,18 @@ async def time(ctx, sub = None):
     jst_time = datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=9))).strftime("%H:%M:%S")
     est_date = datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=-5))).strftime("%Y/%m/%d")
     est_time = datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=-5))).strftime("%H:%M:%S")
+    tst_date = datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=8))).strftime("%Y/%m/%d")
+    tst_time = datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=9))).strftime("%H:%M:%S")
     utc_date = datetime.datetime.now(datetime.timezone.utc).strftime("%Y/%m/%d")
     utc_time = datetime.datetime.now(datetime.timezone.utc).strftime("%H:%M:%S")
     if sub == 'date':
-        embed = discord.Embed(title='📅Date', description=f'UTC `{utc_date}`\n\nJST `{jst_date}`\n\nEST `{est_date}`')
+        embed = discord.Embed(title='📅Date', description=f'UTC `{utc_date}`\n\nTST `{tst_date}`JST `{jst_date}`\n\nEST `{est_date}`')
         await ctx.send(embed=embed)
     elif sub == 'time':
-        embed = discord.Embed(title='⏲Time', description=f'UTC `{utc_time}`\n\nJST `{jst_time}`\n\nEST `{est_time}`')
+        embed = discord.Embed(title='⏲Time', description=f'UTC `{utc_time}`\n\nTST: `{tst_time}`\n\nJST `{jst_time}`\n\nEST `{est_time}`')
         await ctx.send(embed=embed)
     else:
-        embed = discord.Embed(title='📅Date&Time⏲', description=f'UTC `{utc_date} {utc_time}`\n\nJST `{jst_date} {jst_time}`\n\nEST `{est_date} {est_time}`')
+        embed = discord.Embed(title='📅Date&Time⏲', description=f'UTC `{utc_date} {utc_time}`\n\nTST: `{tst_date} {tst_time}`JST `{jst_date} {jst_time}`\n\nEST `{est_date} {est_time}`')
         await ctx.send(embed=embed)
 
 
