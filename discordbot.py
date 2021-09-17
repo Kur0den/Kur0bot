3b1c8832a16bea1405f51f6f5866f83880f1e0b6
Kur0den <kame241612@gmail.com>
2021-09-17 10:35:21 +0900

    わすれてた

--- discordbot.py
+++ discordbot.py
@@ -118,7 +118,7 @@ async def idinfo(ctx, imid):
     await ctx.send(f'{ex_name}\n{tid}')
 
 
-@slash_bot.slash(
+@slash.slash(
     name="test",
     description="てすとだよ",
     guild_id=733707710784340100
