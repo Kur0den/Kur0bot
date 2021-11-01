f565ff9a03bdabf00dfbec0c733cb7f0763d9869
Kur0den <kame241612@gmail.com>
2021-11-01 20:38:45 +0900

    てへ

--- discordbot.py
+++ discordbot.py
@@ -88,7 +88,7 @@ async def on_thread_join(thread):
             # await sent.delete())
             # thnotice = bot.get_channel(733707711228674102)
         
-            await thnotice.send(f'スレッドが作成されたよ！\nスレッド名: {thread.name}\nスレッドID: {thread.id}\nスレッドが作成されたチャンネル: {thread.parent}')
+        await thnotice.send(f'スレッドが作成されたよ！\nスレッド名: {thread.name}\nスレッドID: {thread.id}\nスレッドが作成されたチャンネル: {thread.parent}')
 
 # evalもどき
 @bot.command(hidden = True)
