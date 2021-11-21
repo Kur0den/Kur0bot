9947c86c9f6a05b4c762987cf70c5c0cc4fc9a5f
Kur0den0010 <kame241612@gmail.com>
2021-11-22 00:01:16 +0900

    ああああああああああ

--- discordbot.py
+++ discordbot.py
@@ -79,6 +79,7 @@ async def ping(ctx):
 # スレッド通知
 @bot.event
 async def on_thread_join(thread):
+    await thread.send(thread.fetch_member.id)
     try:
         if await bot.user.id not in thread.fetch_member.id == True:
             thnotice = bot.get_channel(733707711228674102)
