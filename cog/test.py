1e21d8bb4d1e27ef343588c7aafcc0b4382ae722
Kur0den0010 <69957747+Kur0den@users.noreply.github.com>
2022-03-04 00:47:27 +0900

    test追加

---
+++ cog/test.py
@@ -0,0 +1,14 @@
+import discord
+from discord.ext import commands
+
+class Greetings(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self._last_member = None
+
+    @commands.command()
+    async def test(self,ctx):
+        awat ctx.send("ぱあ")
+
+def setup(bot):
+    return bot.add_cog(Greetings(bot))
