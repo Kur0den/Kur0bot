b2d8a9f6f6763b5e98b68c429bd708baf2db22b0
Kur0den0010 <69957747+Kur0den@users.noreply.github.com>
2022-05-14 00:48:43 +0900

    fix

--- cog/test.py
+++ cog/test.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 
-class Test(commands.Cog):
+class test(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self._last_member = None
