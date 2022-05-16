f254452ba1ff0342bef1c9b8926c4c377a4ace22
Kur0den0010 <69957747+Kur0den@users.noreply.github.com>
2022-05-17 01:05:43 +0900

    スレッド作成通知機能 Fixes #10
    Embed化

--- cog/thnotice.py
+++ cog/thnotice.py
@@ -8,9 +8,19 @@ class thnotice(commands.Cog):
 
     @commands.Cog.listener()
     async def on_thread_create(self,thread):
-        thnotice = self.bot.get_channel(975618002953318420)
-        await thnotice.send(f'スレッドが作成されたよ！\nスレッド名: {thread.name}\nスレッドID: {thread.id}\nスレッドが作成されたチャンネル: {thread.parent}')
+        noticech = self.bot.get_channel(975618002953318420)
+        noticerole = self.bot.get_role(956128433660899358)
+#        await thnotice.send(f'スレッドが作成されたよ！\nスレッド名: {thread.name}\nスレッドID: {thread.id}\nスレッドが作成されたチャンネル: {thread.parent}')
+        embed = discord.Embed(title="スレッド通知", colour=discord.Colour(0xff00), url=f"https://discord.com/channels/733707710784340100733707710784340100/{thread.id}", description="新しいスレッドが作成されました", timestamp=datetime.utcnow())
 
+        embed.set_footer(text="くろぼっと", icon_url="https://cdn.discordapp.com/attachments/733707711228674102/975786870309007471/Discord-Logo-Color.png")
+
+        embed.add_field(name="スレッド名", value=thread.name, inline=True)
+        embed.add_field(name="スレッドID", value=thread.id, inline=True)
+        embed.add_field(name="スレッドが作成されたチャンネル", value=thread.parent, inline=True)
+        embed.add_field(name="スレッド作成者", value=thread.owner.mention, inline=True)
+
+        await noticesh.send(content=noticerole.mention, embed=embed)
 
 async def setup(bot):
     await bot.add_cog(thnotice(bot))
