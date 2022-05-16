449ddd08d958b2c27ce174d71f80c3ee17aeef00
Kur0den0010 <69957747+Kur0den@users.noreply.github.com>
2022-05-17 02:14:36 +0900

    変数名変更

--- discordbot.py
+++ discordbot.py
@@ -17,7 +17,6 @@ bot = commands.Bot(
     activity = discord.Activity(name = 'くろでんのくろでんによるくろでんのためのぼっと', type = discord.ActivityType.playing),
     intents=discord.Intents.all())
 token = 'ODc1OTYxOTczNTk3MTcxNzIy.GLMD1j.Zz19BlU2Il-ocu7ir-QM71feSNDgglWHTCtCrE'
-guild = None
 guild_id = [733707710784340100]
 
 
@@ -26,10 +25,10 @@ async def on_ready():
     global guild, unei_members, osirase_ch, osirase_role
     bot.guild = bot.get_guild(733707710784340100)
     botowner = bot.get_user(699414261075804201)
-    unei_role = guild.get_role(738956776258535575)
+    unei_role = bot.guild.get_role(738956776258535575)
     unei_members = unei_role.members
     osirase_ch = bot.get_channel(734605726491607091)
-    osirase_role = guild.get_role(738954587922235422)
+    osirase_role = bot.guild.get_role(738954587922235422)
     login_ch = bot.get_channel(888416525579612230)
     for file in os.listdir('./cog'):
         if file.endswith('.py'):
