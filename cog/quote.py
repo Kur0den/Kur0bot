6e230cfe7f2935bc66b5fc7b5cab5e47ab2c7e4f
Kur0den0010 <69957747+Kur0den@users.noreply.github.com>
2022-05-23 21:08:45 +0900

    修正

--- cog/quote.py
+++ cog/quote.py
@@ -2,15 +2,15 @@ import discord
 from discord.ext import commands
 
 
-class Quote(commands.Cog):
+class quote(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.Cog.listener()
     async def on_message(self, message):
-        QuoteBot = guild.get_member(949479338275913799)
+        QuoteBot = self.bot.guild.get_member(949479338275913799)
         if message.author == QuoteBot:
             await message.channel.send('おでん')
 
 async def setup(bot):
-    await bot.add_cog(Quote(bot))
+    await bot.add_cog(quote(bot))
