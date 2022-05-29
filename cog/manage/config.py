5b85b1becac369c922faf01879cb95c652b8ebcc
Kur0den0010 <69957747+Kur0den@users.noreply.github.com>
2022-05-30 00:05:39 +0900

    コマンド変更

--- cog/manage/config.py
+++ cog/manage/config.py
@@ -8,14 +8,19 @@ class config(commands.Cog):
 
     @commands.command()
     async def config(self, ctx, mode = None, configname = None, argument = None):
-        if mode == 'read':
+        if mode == 'list':
             config = json.dumps(self.bot.config)
             await ctx.send(config)
-        elif mode == 'write':
+        elif mode == 'set':
             if configname  is not None and argument is not None:
                 await ctx.send('テスト成功！')
             else:
                 await ctx.send('引数がおかしいよ\n設定を変更するconfigの名前と数値をいれてね')
+        elif mode == 'reload':
+            with open('config.json', 'r+', encoding='utf-8') as file:
+                bot.config = load(file)
+            print('Config loaded')
+            await ctx.send('configを再読み込みしたよ')
         else:
             await ctx.send('モードがおかしいよ\n`read`か`write`で指定してね')
         
