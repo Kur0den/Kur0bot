2503bb350d63c1221881f312ef3135e54b6c2725
Kur0den0010 <69957747+Kur0den@users.noreply.github.com>
2022-05-31 02:18:24 +0900

    一定時間ごとに講堂をチェックするように

--- cog/money/stagemoney.py
+++ cog/money/stagemoney.py
@@ -1,18 +1,19 @@
 import discord
 from discord.ext import commands
 from discord.ext.tasks import loop
-
+import aiohttp
 
 class stagemoney(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-
-    @loop(seconds=60)
+        self.stage_check.start()
+    
+    @loop(seconds=10.0)
     async def stage_check(self):
         #configロード
         stage_money_min = self.bot.config['stage_money_min']
         stage_money_max = self.bot.config['stage_money_max']
-        print('講堂チェック')
+        print(f'講堂チェック\nStageTopic: {self.bot.stage.topic}')
         #ステージやってるか判定(放置対策)
         if self.bot.stage.topic is not None:
             for member in self.bot.stage.members:
@@ -20,8 +21,15 @@ class stagemoney(commands.Cog):
                 if member.bot is False:
                     #報酬ランダム化
                     money = random.randint(stage_money_min, stage_money_max)
-                    async with aiohttp.ClientSession(headers=self.bot.ub_header) as session:
-                            await session.patch(url=f'{self.bot.ub_url}{member.id}', json={'cash':money, 'reason': f'ステージチャンネル報酬'})
+                    print('テスト成功')
+                    #async with aiohttp.ClientSession(headers=self.bot.ub_header) as session:
+                    #        await session.patch(url=f'{self.bot.ub_url}{member.id}', json={'cash':money, 'reason': f'ステージチャンネル報酬'})
+
+
+    
+    
+
+        
 
 async def setup(bot):
     await bot.add_cog(stagemoney(bot))
