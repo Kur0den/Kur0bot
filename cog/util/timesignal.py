01c1968b2554ca92aa1e11c4a7196c5497e8cfb9
Kur0den0010 <69957747+Kur0den@users.noreply.github.com>
2022-06-17 20:09:23 +0900

    おそらくこれで完成

--- cog/util/timesignal.py
+++ cog/util/timesignal.py
@@ -8,46 +8,46 @@ class timesignal(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.timesignal.start()
-        self.send = None
         self.message = None
+        self.embed = None
     
     
 
     @tasks.loop(minutes=1)
     async def timesignal(self):
-        embed = None
         now = datetime.now().strftime('%H')
         if datetime.now().strftime('%M') == '00':
             
-            embed = discord.Embed(title='時報', colour=discord.Colour(0x4b78e6), description=f'{now}時ちょうどをお知らせします')
+            self.embed = discord.Embed(title='時報', colour=discord.Colour(0x4b78e6), description=f'{now}時ちょうどをお知らせします')
 
-            embed.set_thumbnail(url='https://cdn.discordapp.com/attachments/733707711228674102/986178408696393768/spin.gif')
+            self.embed.set_thumbnail(url='https://cdn.discordapp.com/attachments/733707711228674102/986178408696393768/spin.gif')
             
             if now =='00':
-                embed.add_field(name = 'あけおめ！！！！！！！',value=f'今日は{datetime.now().strftime("%m月%d日")}だよ！！！！！！')
+                self.embed.add_field(name = 'あけおめ！！！！！！！',value=f'今日は{datetime.now().strftime("%m月%d日")}だよ！！！！！！')
             elif now == '12':
-                embed.add_field(name = 'おひるだ！！！！！！！',value=f'ご飯を食べよう！！！！！！！！！')
+                self.embed.add_field(name = 'おひるだ！！！！！！！',value=f'ご飯を食べよう！！！！！！！！！')
             elif now == '15':
-                embed.add_field(name = 'おやつ！！！！！！！',value=f'おやつ！！！！！！！！！')
+                self.embed.add_field(name = 'おやつ！！！！！！！',value=f'おやつ！！！！！！！！！')
             elif now == '23':
-                    embed.add_field(name = 'よるだぞ！！！！！！！',value=f'そろそろ寝よう！！！！！！！！！')
+                    self.embed.add_field(name = 'よるだぞ！！！！！！！',value=f'そろそろ寝よう！！！！！！！！！')
             print(f'時報({now}時)')
             
         elif datetime.now().strftime('%M') == '30':
-                embed = discord.Embed(title='時報', colour=discord.Colour(0x4b78e6), description=f'{now}時30分をお知らせします')
+                self.embed = discord.Embed(title='時報', colour=discord.Colour(0x4b78e6), description=f'{now}時30分をお知らせします')
 
-                embed.set_thumbnail(url='https://cdn.discordapp.com/attachments/733707711228674102/986178408696393768/spin.gif')
+                self.embed.set_thumbnail(url='https://cdn.discordapp.com/attachments/733707711228674102/986178408696393768/spin.gif')
                 
                 print(f'時報({now}時半)')
         
-        if self.send == False:
+        if self.embed != None:
             if self.message != None:
                 await self.message.delete()
-                self.message = await self.bot.guild.system_channel.send(embed=embed)
-                self.send = True
+                self.message = await self.bot.guild.system_channel.send(embed=self.embed)
+                self.embed = None
 
-        if datetime.now().strftime('%M') == '01' or '11' or '21' or '31' or '41' or '51':
-            self.send =  False
+    async def cog_unload(self):
+            self.timesignal.stop()
+
 
 async def setup(bot):
     await bot.add_cog(timesignal(bot))
