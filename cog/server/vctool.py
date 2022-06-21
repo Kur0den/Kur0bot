99cb6e20d4715af243a651b2c020a7983f9f7aaa
Kur0den0010 <69957747+Kur0den@users.noreply.github.com>
2022-06-22 00:08:54 +0900

    VCダッシュボード Fixes #32 オーナー退出時の処理を追加

--- cog/server/vctool.py
+++ cog/server/vctool.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 from datetime import datetime
+import random
 
 
 
@@ -13,7 +14,8 @@ class owner():
         self.vcowner = None
         self.bot = bot
     
-    async def setup(self, member, after, vcowner):
+    # オーナー設定
+    async def setup(self, member, after, result):
         if len(after.channel.members) == 1:
             if after.channel == self.bot.vc1:
                 self.bot.vc1_owner = member
@@ -28,7 +30,8 @@ class owner():
                 vcowner = True
                 return vcowner
     
-    async def check(self, interaction, result):
+    # ボタン用オーナーチェック
+    async def buttoncheck(self, interaction, result):
         if interaction.channel == self.bot.bot.vc1 and interaction.user == self.bot.bot.vc1_owner:
             result = 'vc1'
             return result
@@ -41,17 +44,53 @@ class owner():
         else:
             result = None
             return result
+    
+    # それ以外用オーナーチェック
+    async def usercheck(self, member, before, result):
+        if before.channel == self.bot.vc1 and member == self.bot.vc1_owner:
+            result = 'vc1'
+            return result
+        elif before.channel == self.bot.vc2 and member == self.bot.vc2_owner:
+            result = 'vc2'
+            return result
+        elif before.channel == self.bot.vc3 and member == self.bot.vc3_owner:
+            result = 'vc3'
+            return result
+        else:
+            result = None
+            return result
+    
+    # オーナー変更
+    async def change(self, channel, result):
+        member = channel.members
+        count = 0
+        for user in member:
+            if user.bot == True:
+                member.pop(count)
+            count + 1
+        
+        if channel == self.bot.vc1:
+            self.bot.vc1_owner = random.choice(member)
+            await channel.send(f'{self.bot.vc1_owner.mention}は{channel}の所有権を持っています', delete_after=60)
+        elif channel == self.bot.vc2:
+            self.bot.vc2_owner = random.choice(member)
+            await channel.send(f'{self.bot.vc2_owner.mention}は{channel}の所有権を持っています', delete_after=60)
+        elif channel == self.bot.vc3:
+            self.bot.vc3_owner = random.choice(member)
+            await channel.send(f'{self.bot.vc3_owner.mention}は{channel}の所有権を持っています', delete_after=60)
+
 
 
 class dashboard(discord.ui.View):
     def __init__(self, bot):
         super().__init__()
         self.bot = bot
+        discord.ui.view.timeout = None
     
-    @discord.ui.button(label='ロック', style=discord.ButtonStyle.green, emoji='🔒')
+    @discord.ui.button(label='ロック', style=discord.ButtonStyle.green, emoji='🔒', row=1)
     async def lock(self, interaction: discord.Interaction, button: discord.ui.Button):
         result = None
-        await owner.check(self, interaction, result)
+        result = await owner.buttoncheck(self, interaction, result)
         if result == 'vc1':
             await interaction.response.send_message('vc1', ephemeral=True)
         elif result == 'vc2':
@@ -78,14 +117,7 @@ class vctool(commands.Cog):
         self.bot = bot
         
 
-    @commands.command()
-    async def senddash(self, ctx):
-        if (ctx.channel is self.bot.vc1 or
-            ctx.channel is self.bot.vc2 or
-            ctx.channel is self.bot.vc3):
-            await ctx.send('test', view=dashboard(self))
-        else:
-            await ctx.send('チャンネルが違うで')
+
     
 
     @commands.Cog.listener()
@@ -103,18 +135,8 @@ class vctool(commands.Cog):
             if before.channel != after.channel:
                 # 退出
                 if before.channel is not None and before.channel != stage:
-                    #if before.channel == vc1:
-                    #    async for msg in log1.history():
-                    #        if msg.content.startswith(str(member.id)):
-                    #            await msg.delete()
-                    #elif before.channel == log2:
-                    #    async for msg in vc2.history():
-                    #        if msg.content.startswith(str(member.id)):
-                    #            await msg.delete()
-                    #elif before.channel == log3:
-                    #    async for msg in vc3.history():
-                    #        if msg.content.startswith(str(member.id)):
-                    #            await msg.delete()
+
+
 
                     embed = discord.Embed(title="VC退出", colour=discord.Colour(0xd0021b), description="ユーザーが退出しました", timestamp=datetime.now())
 
@@ -127,11 +149,17 @@ class vctool(commands.Cog):
                         msg = await before.channel.send(embed=discord.Embed(title='チャンネルリセット中...', description='VCに誰もいなくなったためチャンネルをリセットしています', color=0x00ffff))
                         await before.channel.purge(limit=None, check=purge_check)
                         await msg.delete()
+                    else:
+                        result = await owner.usercheck(self, member, before, None)
+                    
+                        if result != None:
+                            await owner.change(self, before.channel, None)
                     
                 # 入室
                 if after.channel is not None and after.channel != stage:
                     # オーナー指定
-                    await owner.setup(self, member, after)
+                    
+                    result = await owner.setup(self, member, after, None)
                     
                     embed = discord.Embed(title = "VC入室", colour = discord.Colour(0x7ed321), description = "ユーザーが入室しました", timestamp = datetime.now())
 
@@ -140,8 +168,9 @@ class vctool(commands.Cog):
 
                     await after.channel.send(embed=embed)
                     
-                    if owner.vcowner == True:
-                        await ctx.send(f'{member.mention}は{after.channel}の所有権を持っています')
+                    if  result == True:
+                        await after.channel.send('test', view=dashboard(self))
+                        await after.channel.send(f'{member.mention}は{after.channel}の所有権を持っています', delete_after=60)
 
             
 
