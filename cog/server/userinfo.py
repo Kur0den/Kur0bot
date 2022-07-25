29e15ba6f97467d759113a6e449b6b9562962df4
aicy <rx435423@gmail.com>
2022-07-26 01:10:36 +0900

    ゆーざーいんふぉとりあえずついか

---
+++ cog/server/userinfo.py
@@ -0,0 +1,26 @@
+import discord
+from discord.ext import commands
+import requests
+
+
+
+
+
+
+class userinfo(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def userinfo(ctx, member : discord.Member):
+        e = discord.Embed(title=f'{member}の詳細', description='詳細だよ', color=discord.Color.orange())
+        e.add_field(name='名前', value=f'{member}')
+        e.add_field(name='あなたはBot?', value=member.bot)
+        # e.add_field(name='ID', value=member.id)
+        e.add_field(name='作成時間', value=member.created_at)
+        e.add_field(name='サーバーに参加した時間', value=member.joined_at)
+        await ctx.send(embed=e)
+
+
+async def setup(bot):
+    await bot.add_cog(userinfo(bot))
