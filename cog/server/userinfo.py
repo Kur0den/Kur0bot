5b4ffbc1bb98f0b74c064588ae337398a96de65c
Kur0den0010 <69957747+Kur0den@users.noreply.github.com>
2022-07-28 16:59:32 +0900

    べるさんsコード

--- cog/server/userinfo.py
+++ cog/server/userinfo.py
@@ -8,26 +8,38 @@ class userinfo(commands.Cog):
         self.bot = bot
 
     @commands.command(aliases=['ui'])
-    async def userinfo(self, ctx, *, user: discord.User = None):
+    async def userinfo(self, ctx, user: discord.User = None):
         if user == None:
             user = ctx.author
-        member = discord.utils.get(self.bot.get_all_members(), id=user.id)
-        e = discord.Embed(title=f'{user}の詳細', description='詳細だよ', color=user.color)
-        e.add_field(name='名前', value=user)
-        e.add_field(name='Botかどうか', value=user.bot)
-        e.add_field(name='ID', value=user.id)
-        g_m = discord.utils.get(ctx.guild.members, id=user.id)
-        e.set_thumbnail(url=user.avatar)
-        dt_now_jst_aware = timezone(timedelta(hours=+9), 'JST')
-        jointime = member.joined_at.astimezone(dt_now_jst_aware).strftime(
-            "%Y年%m月%d日%H時%M分%S秒"
+
+
+        member = ctx.guild.get_member(user.id)
+        if member is not None:
+            user = member
+
+        embed = discord.Embed(title=f'{user}の詳細', description='詳細だよ', color=user.color)
+
+        embed.add_field(name='名前', value=user)
+        embed.add_field(name='Botかどうか', value=user.bot)
+        embed.add_field(name='ID', value=user.id)
+
+        embed.set_thumbnail(url=user.avatar)
+
+        jst = timezone(timedelta(hours=9))
+
+
+        if member is not None:
+            joined_at = member.joined_at.astimezone(jst).strftime(
+                "%Y/%m/%d %H:%M:%S"
+            )
+            embed.add_field(name="サーバー参加日", value=joined_at, inline=False)
+
+        created_at = user.created_at.astimezone(jst).strftime(
+        "%Y/%m/%d %H:%M:%S"
         )
-        e.add_field(name="サーバー参加日", value=jointime, inline=False)
-        createtime = user.created_at.astimezone(dt_now_jst_aware).strftime(
-        "%Y年%m月%d日%H時%M分%S秒"
-        )
-        e.add_field(name="アカウント作成日", value=createtime, inline=False)
-        await ctx.send(embed=e)
+        embed.add_field(name="アカウント作成日", value=created_at, inline=False)
+
+        await ctx.replay(embed=embed)
 
 
 async def setup(bot):
