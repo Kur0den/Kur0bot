16c3e55fd3be8c8d538220ab0a1e1b25c78f0d7a
Kur0den0010 <69957747+Kur0den@users.noreply.github.com>
2022-07-31 19:15:05 +0900

    Revert "members取得できた！！！(数値"
    This reverts commit f26001d4f1d2fb2cc32c21fada6d00baabe68959.

--- cog/server/vctool.py
+++ cog/server/vctool.py
@@ -414,32 +414,30 @@ class dashboard(discord.ui.View):
     @discord.ui.button(label='VCの情報', style=discord.ButtonStyle.secondary, emoji='ℹ', row=4)
     async def info(self, interaction: discord.Integration, button: discord.ui.Button):
         if interaction.channel == self.bot.vc1:
-            members = [i.name for i in self.bot.vc1.members]
             embed = discord.Embed(title='VC1の情報', description='')
             embed.add_field(name='名前', value=self.bot.vc1.name)
             embed.add_field(name='オーナー', value=self.bot.vc1_owner)
             embed.add_field(name='状態', value=self.bot.vc1_status)
-            embed.add_field(name='何人いるか(Bot再起動などで正常に取得できてない場合があります。)', value=len(members))
+            embed.add_field(name='何人いるか(Bot再起動などで正常に取得できてない場合があります。)', value=len(self.bot.vc2_members))
             embed.add_field(name='NSFWかどうか', value=self.bot.vc1.nsfw)
-            await interaction.response.send_message(embed=embed)
+            await interaction.response.send_message(embed=embed, delete_after=60)
         elif interaction.channel == self.bot.vc2:
-            members = [i.name for i in self.bot.vc2.members]
             embed = discord.Embed(title='VC2の情報', description='')
             embed.add_field(name='名前', value=self.bot.vc2.name)
             embed.add_field(name='オーナー', value=self.bot.vc2_owner)
             embed.add_field(name='状態', value=self.bot.vc2_status)
-            embed.add_field(name='何人いるか(Bot再起動などで正常に取得できてない場合があります。)', value=len(members))
-            embed.add_field(name='NSFWかどうか', value=self.bot.vc2.nsfw)
-            await interaction.response.send_message(embed=embed)
+            embed.add_field(name='何人いるか(Bot再起動などで正常に取得できてない場合があります。)', value=len(self.bot.vc2_members))
+            embed.add_field(name='NSFWかどうか', value=self.bot.vc2_nsfw)
+            await interaction.response.send_message(embed=embed, delete_after=60)
         elif interaction.channel == self.bot.vc3:
-            members = [i.name for i in self.bot.vc3.members]
             embed = discord.Embed(title='VC3の情報', description='')
-            embed.add_field(name='名前', value=self.bot.vc3.name)
-            embed.add_field(name='オーナー', value=self.bot.vc3_owner)
-            embed.add_field(name='状態', value=self.bot.vc3_status)
-            embed.add_field(name='何人いるか(Bot再起動などで正常に取得できてない場合があります。)', value=len(members))
-            embed.add_field(name='NSFWかどうか', value=self.bot.vc3.nsfw)
-            await interaction.response.send_message(embed=embed)
+            embed.add_field(name='名前', value=self.bot.vc2.name)
+            embed.add_field(name='オーナー', value=self.bot.vc2_owner)
+            embed.add_field(name='状態', value=self.bot.vc2_status)
+            embed.add_field(name='何人いるか(Bot再起動などで正常に取得できてない場合があります。)', value=len(self.bot.vc2_members))
+            embed.add_field(name='NSFWかどうか', value=self.bot.vc2_nsfw)
+            await interaction.response.send_message(embed=embed, delete_after=60)
+
 
 
 class vctool(commands.Cog):
