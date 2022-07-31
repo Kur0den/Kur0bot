723e9e794846a8258fdad078eba3a1ffe79e8630
Kur0den0010 <69957747+Kur0den@users.noreply.github.com>
2022-07-31 22:57:34 +0900

    取得できてないのを修正

--- cog/server/vctool.py
+++ cog/server/vctool.py
@@ -418,7 +418,7 @@ class dashboard(discord.ui.View):
             embed.add_field(name='名前', value=self.bot.vc1.name)
             embed.add_field(name='オーナー', value=self.bot.vc1_owner)
             embed.add_field(name='状態', value=self.bot.vc1_status)
-            embed.add_field(name='何人いるか(Bot再起動などで正常に取得できてない場合があります。)', value=len(self.bot.vc2_members))
+            embed.add_field(name='何人いるか(Bot再起動などで正常に取得できてない場合があります。)', value=len(self.bot.vc1.members))
             embed.add_field(name='NSFWかどうか', value=self.bot.vc1.nsfw)
             await interaction.response.send_message(embed=embed, delete_after=60)
         elif interaction.channel == self.bot.vc2:
@@ -426,7 +426,7 @@ class dashboard(discord.ui.View):
             embed.add_field(name='名前', value=self.bot.vc2.name)
             embed.add_field(name='オーナー', value=self.bot.vc2_owner)
             embed.add_field(name='状態', value=self.bot.vc2_status)
-            embed.add_field(name='何人いるか(Bot再起動などで正常に取得できてない場合があります。)', value=len(self.bot.vc2_members))
+            embed.add_field(name='何人いるか(Bot再起動などで正常に取得できてない場合があります。)', value=len(self.bot.vc2.members))
             embed.add_field(name='NSFWかどうか', value=self.bot.vc2_nsfw)
             await interaction.response.send_message(embed=embed, delete_after=60)
         elif interaction.channel == self.bot.vc3:
@@ -434,9 +434,9 @@ class dashboard(discord.ui.View):
             embed.add_field(name='名前', value=self.bot.vc2.name)
             embed.add_field(name='オーナー', value=self.bot.vc2_owner)
             embed.add_field(name='状態', value=self.bot.vc2_status)
-            embed.add_field(name='何人いるか(Bot再起動などで正常に取得できてない場合があります。)', value=len(self.bot.vc2_members))
+            embed.add_field(name='何人いるか(Bot再起動などで正常に取得できてない場合があります。)', value=len(self.bot.vc3.members))
             embed.add_field(name='NSFWかどうか', value=self.bot.vc2_nsfw)
-            await interaction.response.send_message(embed=embed, delete_after=60)
+            message = await interaction.response.send_message(embed=embed)
 
 
 
