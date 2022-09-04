77538bd91416e0881ad4bb037f5d84cad87dc381
Kur0den0010 <69957747+Kur0den@users.noreply.github.com>
2022-09-04 20:23:08 +0900

    テスト手とりあえず書いた

--- cog/server/profile.py
+++ cog/server/profile.py
@@ -6,7 +6,13 @@ class profile(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    
+    @commands.command(name="set")
+    async def set_profile(ctx, *, text):  # setだと変数名がかぶるので変更、*,で空白を無視
+        await profiles_collection.insert_one({
+            "userid": ctx.author.id,
+        "text": text
+        })
+        await ctx.reply("設定が完了しました。")
 
-def setup(bot):
-    bot.add_cog(profile(bot))
+async def setup(bot):
+    await bot.add_cog(profile(bot))
