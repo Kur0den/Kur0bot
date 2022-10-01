c0822aaab4b0fbe00442d2711457a3b20f8436b1
Kur0den0010 <69957747+Kur0den@users.noreply.github.com>
2022-10-01 18:30:05 +0900

    枠組みが完成

--- cog/server/tts.py
+++ cog/server/tts.py
@@ -10,28 +10,44 @@ from gtts import gTTS
 class tts(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.vc = None
 
     group = app_commands.Group(name="tts", description="Text To Speech", guild_ids=[733707710784340100], guild_only=True)
 
-@group.command()
-@app_commands.guild_only()
-async def join(self, interaction: discord.Interaction):
-    
-    await interaction.response.send_message()
+    @group.command(name='connect', description='VCに接続します')
+    @app_commands.guild_only()
+    async def join(self, interaction: discord.Interaction):
+        if interaction.channel is (self.bot.vc1 or saelf.bot.vc2 or self.bot.vc3):
+            if self.bot.voice_clients == []:
+                if interaction.user.voice.channel is interaction.channel:
+                    self.vc = await interaction.channel.connect()
+                    await interaction.response.send_message('接続しました')
+                    return
+        await interaction.response.send_message('接続に失敗しました\nこのコマンドは接続しているVCの聞き専チャンネルで使用してください')
 
 
-@group.command()
-@app_commands.guild_only()
-async def leave(self, interaction: discord.Interaction):
-    
-    await interaction.response.send_message()
+    @group.command(name='disconnect', description='VCから切断します')
+    @app_commands.guild_only()
+    async def leave(self, interaction: discord.Interaction):
+        if self.vc != None:
+            if interaction.channel is self.vc.channel:
+                if interaction.user.voice.channel is self.vc.channel:
+                    await self.vc.disconnect()
+                    self.vc = None
+                    await interaction.response.send_message('切断しました')
+                    return
+        await interaction.response.send_message('失敗しました')
 
 
 
 
-# メッセージ取得
-async def on_message(self, message):
-    if message.channel.type is (self.bot.vc1 or self.bot.vc2 or self.bot.vc3):
+    # メッセージ取得
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if self.vc != None:
+            if message.author.bot is False:
+                if message.channel is self.vc.channel:
+                    await message.channel.send('じゃーん')
 
 
 async def setup(bot: commands.Bot) -> None:
