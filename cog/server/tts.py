2f7681f359d56e0f253f272f220fe2024265e12c
69957747+Kur0den@users.noreply.github.com <69957747+Kur0den@users.noreply.github.com>
2022-12-17 17:16:56 +0900

    少し修正

--- cog/server/tts.py
+++ cog/server/tts.py
@@ -105,13 +105,11 @@ class tts(commands.Cog):
                     name = uuid.uuid1()
                     g_tts.save(f'./.tts_voice/{name}.mp3')
                     self.bot.guild.voice_client.play(discord.FFmpegPCMAudio(f"./.tts_voice/{name}.mp3"))
-
-    @commands.Cog.listener()
-    async def on_voice_state_update(self, member, before, after):
-        if member.id is self.bot.user.id:
+        elif member.id is self.bot.user.id:
             if before.channel is not None:
                 shutil.rmtree(self.bot.tts_file)
                 os.mkdir(self.bot.tts_file)
+
 
 async def setup(bot: commands.Bot) -> None:
     await bot.add_cog(tts(bot))
