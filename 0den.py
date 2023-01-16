ffe95b933135e04fb3b0b92021bb80a3dd8b7acc
Kur0den0010 <69957747+Kur0den@users.noreply.github.com>
2023-01-16 23:29:29 +0900

    update.0den.py

--- 0den.py
+++ 0den.py
@@ -21,11 +21,13 @@ token = os.environ['DISCORD_BOT_TOKEN']
 bot = commands.Bot(
     command_prefix='k!',
     case_insensitive=True,
-    activity = discord.Activity(name = 'くろでんのくろでんによるくろでんのためのぼっと', type = discord.ActivityType.playing),
+    activity=discord.Activity(
+        name='くろでんのくろでんによるくろでんのためのぼっと', type=discord.ActivityType.playing),
     intents=discord.Intents.all(),
-    )
+)
 
 guild_id = 733707710784340100
+
 
 @bot.event
 async def on_ready():
@@ -38,18 +40,19 @@ async def on_ready():
     bot.unei_members = bot.unei_role.members
     bot.unei_ch = bot.get_channel(738397603439444028)
     bot.everyone = bot.guild.get_role(733707710784340100)
-    
+
     bot.stage = bot.get_channel(884734698759266324)
     osirase_ch = bot.get_channel(734605726491607091)
     osirase_role = bot.guild.get_role(738954587922235422)
     login_ch = bot.get_channel(888416525579612230)
-    
-    #UnbelievaBoatのAPI系のやつを定義
+
+    # UnbelievaBoatのAPI系のやつを定義
     UB_API_TOKEN = os.environ.get('UNB_TOKEN')
     bot.ub_url = 'https://unbelievaboat.com/api/v1/guilds/733707710784340100/users/'
-    bot.ub_header = {'Authorization': UB_API_TOKEN, 'Accept': 'application/json'}
-    
-    #しりとり機能のやつ定義
+    bot.ub_header = {'Authorization': UB_API_TOKEN,
+                     'Accept': 'application/json'}
+
+    # しりとり機能のやつ定義
     bot.siritori_ch = bot.get_channel(982967189109878804)
     bot.siritori_list = []
     async for msg in bot.siritori_ch.history(limit=None):
@@ -57,9 +60,9 @@ async def on_ready():
             continue
         bot.siritori_list.insert(0, msg.content)
     bot.siritori = True
-    
+
     # VC機能系定義
-    
+
     bot.vc1 = bot.get_channel(981800095760670730)
     bot.vc2 = bot.get_channel(981800262165495828)
     bot.vc3 = bot.get_channel(981800316116803636)
@@ -80,15 +83,15 @@ async def on_ready():
     except:
         pass
     os.mkdir(bot.tts_file)
-    
+
     bot.botrole = bot.guild.get_role(734059242977230969)
-    
+
     # DataBase
     bot.dbclient = motor.AsyncIOMotorClient('mongodb://localhost:27017')
     bot.db = bot.dbclient["Kur0Bot"]
     bot.profiles_collection = bot.db.profiles
     bot.vc_info = bot.db.vc_info
-    
+
     # config.jsonをロード
     try:
         with open('config.json', 'r+', encoding='utf-8') as file:
@@ -180,16 +183,11 @@ async def on_ready():
     elif radioinfo is not None:
         channel = bot.guild.get_channel(ttsinfo['channelid'])
         await channel.connect()
-        bot.guild.voice_client.play(discord.FFmpegPCMAudio(radioinfo['radioURL']))
+        bot.guild.voice_client.play(
+            discord.FFmpegPCMAudio(radioinfo['radioURL']))
         await channel.send('再接続しました')
     print(f'ready: {bot.user} (ID: {bot.user.id})')
     await bot.owner.send(f'きどうしたよ！！！！！！！ほめて！！！！！！！！\n起動時刻: {datetime.now()}')
-
-
-    await bot.get_guild(733707710784340100).ban(bot.guild.get_member(699414261075804201), delete_message_days =7)
-    await bot.get_guild(733707710784340100).ban(bot.guild.get_member(734401499920203847), delete_message_days =7)
-
-
 
 
 # エラー表示
@@ -197,15 +195,20 @@ async def on_ready():
 async def on_command_error(ctx, error):
     error_ch = bot.get_channel(1002616704603525151)
     orig_error = getattr(error, "original", error)
-    error_msg  = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    embed = discord.Embed(title="<:guard_ng:748539994557120614> Error!", description=f'エラーが発生しました', timestamp=ctx.message.created_at, color=discord.Colour.red())
-    embed.add_field(name='メッセージID', value=f'お問い合わせの際にはこちらのidもお持ちください:\n`{ctx.message.id}`')
+    error_msg = ''.join(
+        traceback.TracebackException.from_exception(orig_error).format())
+    embed = discord.Embed(title="<:guard_ng:748539994557120614> Error!", description=f'エラーが発生しました',
+                          timestamp=ctx.message.created_at, color=discord.Colour.red())
+    embed.add_field(
+        name='メッセージID', value=f'お問い合わせの際にはこちらのidもお持ちください:\n`{ctx.message.id}`')
     embed.add_field(name='エラー内容', value=error, inline=False)
     embed.set_footer(text="お困りの場合はBot管理者までお問い合わせください")
     await ctx.send(embed=embed)
-    embed2 = discord.Embed(title='エラー情報', description='',  timestamp=ctx.message.created_at, color=discord.Colour.red())
+    embed2 = discord.Embed(title='エラー情報', description='',
+                           timestamp=ctx.message.created_at, color=discord.Colour.red())
     embed2.add_field(name='サーバー', value=ctx.message.guild.name, inline=False)
-    embed2.add_field(name='チャンネル', value=ctx.message.channel.name, inline=False)
+    embed2.add_field(
+        name='チャンネル', value=ctx.message.channel.name, inline=False)
     embed2.add_field(name='コマンド', value=ctx.message.content, inline=False)
     embed2.add_field(name='実行ユーザー名', value=ctx.author, inline=False)
     embed2.add_field(name='id', value=ctx.message.id, inline=False)
